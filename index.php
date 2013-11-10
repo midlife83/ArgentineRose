952b63856fe232a4c3cfc8a2b93f49239f2300c7
rdmelo <a9c9@ugrad.cs.ubc.ca>
2013-11-10 13:56:24 -0800

    Minor update, just some code clean up

--- index.php
+++ index.php
@@ -31,10 +31,10 @@
     <li class="left">
      <a href="#report_issues_tab" data-toggle="tab">Report An Issue</a>
     </li>
-        
-        <li class="right" style="width:300px; text-align:center">
-        <a href="#" id="recentActivityPopOver" rel="popover" data-original-title="Recent Activities">Recent Activities</a>
-        </li>
+	
+	<li class="right" style="width:300px; text-align:center">
+	<a href="#" id="recentActivityPopOver" rel="popover" data-original-title="Recent Activities">Recent Activities</a>
+	</li>
 
    </ul>
   </div>
@@ -42,28 +42,28 @@
   <div id="recentActivity_popover_content_wrapper" style="display: none">
                 <div class="list-group">
                   <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">just now</font></span>
-                                        <font size="1">Mike updated Issue #3252</font>
+					<span class="badge pull-right"><font size="1">just now</font></span>
+					<font size="1">Mike updated Issue #3252</font>
                   </a>
                   <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">23 mins ago</font></span>
-                                        <font size="1">Julie commneted on Issue #2212</font>
+					<span class="badge pull-right"><font size="1">23 mins ago</font></span>
+					<font size="1">Julie commneted on Issue #2212</font>
                   </a>
-                                  <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">48 mins ago</font></span>
-                                        <font size="1">Ashley resolved Issue #1789</font>
+				  <a href="#" class="list-group-item">
+					<span class="badge pull-right"><font size="1">48 mins ago</font></span>
+					<font size="1">Ashley resolved Issue #1789</font>
                   </a>
-                                  <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">1 hour ago</font></span>
-                                        <font size="1">Tom created Issue #4912</font>
+				  <a href="#" class="list-group-item">
+					<span class="badge pull-right"><font size="1">1 hour ago</font></span>
+					<font size="1">Tom created Issue #4912</font>
                   </a>
-                                  <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">just now</font></span>
-                                        <font size="1">Joseph edited Issue #2732</font>
+				  <a href="#" class="list-group-item">
+					<span class="badge pull-right"><font size="1">just now</font></span>
+					<font size="1">Joseph edited Issue #2732</font>
                   </a>
-                                  <a href="#" class="list-group-item">
-                                        <span class="badge pull-right"><font size="1">just now</font></span>
-                                        <font size="1">Michael resolved Issue #1821</font>
+				  <a href="#" class="list-group-item">
+					<span class="badge pull-right"><font size="1">just now</font></span>
+					<font size="1">Michael resolved Issue #1821</font>
                   </a>
                 </div>
                 <div class="text-right">
@@ -87,8 +87,8 @@
     </form>
     <div class="col-sm-2" >
       <div class="list-group student-list">
-        <a id="alyssa-anchor" class="list-group-item active" onclick="displayAlyssa()">Dunn, Alyssa</a>
-        <a id="jeff-anchor" class="list-group-item" onclick="displayJeff()">Jung, Jeff</a>
+        <a href="#alyssa" class="list-group-item" data-toggle="tab">Dunn, Alyssa</a>
+        <a href="#" class="list-group-item active">Jung, Jeff</a>
         <a href="#" class="list-group-item">Morbi leo risus</a>
         <a href="#" class="list-group-item">Porta ac cons</a>
         <a href="#" class="list-group-item">Vestibulum</a>
@@ -106,7 +106,7 @@
         <a href="#" class="list-group-item">Vestibulum</a>
       </div>
     </div>
-    <div class="alyssa col-sm-10" id="alyssa">
+    <div class="alyssa col-sm-10">
       <div class="panel panel-primary">
         <div class="panel-heading">
           <h3 class="panel-title">Alyssa Dunn</h3>
@@ -156,216 +156,49 @@
             </li>
            </ul>
           </div>
-          <div class="tab-content" id="studentTabContent">
-            <div class="tab-pane active" id="unresolved_tab_student">
-             <table class="table table-hover table-striped" id="unresolved_tab_table_student">
-              <tr class="info">
-               <td>Ref#</td>
-               <td>Submitted by</td>
-               <td>Issue Title</td>
-               <td>Date Submitted</td>
-               <td>Student #</td>
-                <td>Student Name</td>
-               <td>Last Activity</td>
-               <td>Label</td>
-              </tr>
-              <?php $filepath="files/unresolved_issues.txt" ; 
-                    if (file_exists($filepath)) 
-                    { 
-                      $file=fopen($filepath, 'r'); 
-                      while (!feof($file)) 
-                      { 
-                        $lines=fgets($file); 
-                        $first_char=$lines[0];
-                        if ($first_char !='*' && $first_char !='^' && trim($lines) !='') 
-                        { 
-                          $split=explode( '|', $lines);
-                          $name = str_replace(' ', '', $split[5]);
-                            if ($name=='AlyssaDunn') { 
-                            echo '<tr class="danger">'; 
-                            foreach($split as $line) 
-                            { 
-                              echo '<td>'.$line. '</td>';
-                            } 
-                            echo '</tr>'; 
-                          }
-                        } 
-                      } fclose($file); 
-                    } 
-              ?>
-             </table>
-          </div>
-          <div class="tab-pane" id="resolved_tab_student">
-           <table class="table table-hover table-striped" id="resolved_tab_table_student">
-            <tr class="info">
-             <td>Ref#</td>
-             <td>Submitted by</td>
-             <td>Issue Title</td>
-             <td>Date Submitted</td>
-             <td>Student #</td>
-             <td>Student Name</td>
-             <td>Last Activity</td>
-             <td>Label</td>
-            </tr>
-            <?php $filepath="files/resolved_issues.txt" ; 
-                  if (file_exists($filepath)) 
-                  { 
-                    $file=fopen($filepath, 'r'); 
-                    while (!feof($file)) 
-                    { 
-                      $lines=fgets($file); 
-                      $first_char=$lines[0];
-                      if ($first_char !='*' && $first_char !='^' && trim($lines) !='') 
-                      { 
-                        $split=explode( '|', $lines);
-                        $name = str_replace(' ', '', $split[5]);
-                          if ($name=='AlyssaDunn') { 
-                          echo '<tr class="success">'; 
-                          foreach($split as $line) 
-                          { 
-                            echo '<td>'.$line. '</td>';
-                          } 
-                          echo '</tr>'; 
-                        }
-                      } 
-                    } fclose($file); 
-                  } 
-            ?>
-            
-          </table>
-          </div>
-         </div>
-        </div>
-      </div>
-    </div> 
-    <div class="alyssa col-sm-10" id="jeff">
-      <div class="panel panel-primary">
-        <div class="panel-heading">
-          <h3 class="panel-title">Jeff Jung</h3>
-        </div>
-        <div class="panel-body">
-          <div class="row">
-            <div class="col-sm-2">
-                <img src="assets/person.png">
-            </div>
-            <div class="col-sm-6">
-              <h4>Name: Jeff Jung</h4>
-              <h5>Student No: 2342342</h5>
-            </div>
-          </div>
-          <div class="row">
-            <table class="grades table table-bordered col-sm-4">
-              <tr class="info">
-               <td>Lab 1</td>
-               <td>Quiz 1</td>
-               <td>Lab 2</td>
-               <td>Lab 3</td>
-               <td>MT 1</td>
-               <td>Lab 4</td>
-               <td>Quiz 2</td>
-               <td>MT 2</td>
-              </tr>
-              <tr>
-              <td class="danger">40%</td>
-              <td>66%</td>
-              <td>50%</td>
-              <td>60%</td>
-              <td class="danger">33%</td>
-              <td class="danger">30%</td>
-              <td>60%</td>
-              <td>60%</td>
-            </table>
-          </div>
-          <h4>Issue History</h4>
-          <div class="row student-issue-sort">
-           <ul class="nav nav-pills col-md-8" style="margin-bottom:0px">
-            <li class="active left">
-             <a href="#unresolved_tab_student_jeff" data-toggle="tab">Unresolved</a>
-            </li>
+              <div class="tab-content" id="studentTabContent">
+                <div class="tab-pane active" id="unresolved_tab_student">
+                 <table class="table table-hover table-striped" id="unresolved_tab_table_student">
+                  <tr class="info">
+                  <td>
+                   <input type="checkbox">
+                  </td>
+                   <td>Ref#</td>
+                   <td>Submitted by</td>
+                   <td>Issue Title</td>
+                   <td>Date Submitted</td>
+                   <td>Student #</td>
+                   <td>Student Name</td>
+                   <td>Last Activity</td>
+                   <td>Label</td>
+				   <?php include("populateUnresolvedIssues.php"); ?> 
+                  </tr>
+                  
+                 </table>
+               <p style="display: none">unresolved_tab</p>
+              </div>
 
-            <li class="left">
-             <a href="#resolved_tab_student_jeff" data-toggle="tab">Resolved</a>
-            </li>
-           </ul>
-          </div>
-          <div class="tab-content" id="studentTabContent">
-            <div class="tab-pane active" id="unresolved_tab_student_jeff">
-             <table class="table table-hover table-striped" id="unresolved_tab_table_student">
-              <tr class="info">
-               <td>Ref#</td>
-               <td>Submitted by</td>
-               <td>Issue Title</td>
-               <td>Date Submitted</td>
-               <td>Student #</td>
-                <td>Student Name</td>
-               <td>Last Activity</td>
-               <td>Label</td>
-              </tr>
-              <?php $filepath="files/unresolved_issues.txt" ; 
-                    if (file_exists($filepath)) 
-                    { 
-                      $file=fopen($filepath, 'r'); 
-                      while (!feof($file)) 
-                      { 
-                        $lines=fgets($file); 
-                        $first_char=$lines[0];
-                        if ($first_char !='*' && $first_char !='^' && trim($lines) !='') 
-                        { 
-                          $split=explode( '|', $lines);
-                          $name = str_replace(' ', '', $split[5]);
-                            if ($name=='JeffJung') { 
-                            echo '<tr class="danger">'; 
-                            foreach($split as $line) 
-                            { 
-                              echo '<td>'.$line. '</td>';
-                            } 
-                            echo '</tr>'; 
-                          }
-                        } 
-                      } fclose($file); 
-                    } 
-              ?>
-             </table>
-          </div>
-          <div class="tab-pane" id="resolved_tab_student_jeff">
-           <table class="table table-hover table-striped" id="resolved_tab_table_student">
-            <tr class="info">
-             <td>Ref#</td>
-             <td>Submitted by</td>
-             <td>Issue Title</td>
-             <td>Date Submitted</td>
-             <td>Student #</td>
-             <td>Student Name</td>
-             <td>Last Activity</td>
-             <td>Label</td>
-            </tr>
-            <?php $filepath="files/resolved_issues.txt" ; 
-                  if (file_exists($filepath)) 
-                  { 
-                    $file=fopen($filepath, 'r'); 
-                    while (!feof($file)) 
-                    { 
-                      $lines=fgets($file); 
-                      $first_char=$lines[0];
-                      if ($first_char !='*' && $first_char !='^' && trim($lines) !='') 
-                      { 
-                        $split=explode( '|', $lines);
-                        $name = str_replace(' ', '', $split[5]);
-                          if ($name=='JeffJung') { 
-                          echo '<tr class="success">'; 
-                          foreach($split as $line) 
-                          { 
-                            echo '<td>'.$line. '</td>';
-                          } 
-                          echo '</tr>'; 
-                        }
-                      } 
-                    } fclose($file); 
-                  } 
-            ?>
-          </table>
-          </div>
-         </div>
+                <div class="tab-pane" id="resolved_tab_student">
+                 <table class="table table-hover table-striped" id="resolved_tab_table_student">
+                  <tr class="info">
+                  <td>
+                   <input type="checkbox">
+                  </td>
+                   <td>Ref#</td>
+                   <td>Submitted by</td>
+                   <td>Issue Title</td>
+                   <td>Date Submitted</td>
+                   <td>Student #</td>
+                   <td>Student Name</td>
+                   <td>Last Activity</td>
+                   <td>Label</td>
+                  </tr>
+              <?php include("populateResolvedIssues.php"); ?> 
+                 </table>
+               <p style="display: none">resolved_tab</p>
+                </div>
+               </div>
+
         </div>
       </div>
     </div>    
@@ -400,13 +233,13 @@
         <a href="#all_tab" data-toggle="tab" onClick="changeToMarkAsBothResolvedAndUnresolved()">All</a>
        </li>
       </ul>
-          
+	  
       <button type="button" class="btn btn-default col-md-4" style="float:right;margin-bottom:0px; margin-top:5px; width:200px" id="markIssueButton" onclick="updateIssuesTable()">Mark As Resolved
-          </button>
+	  </button>
      </div>
 
 
-<div class="tab-content" id="issuesTabContent">
+     <div class="tab-content" id="issuesTabContent">
       <div class="tab-pane active" id="unresolved_tab">
        <table class="table table-hover table-striped" id="unresolved_tab_table">
         <tr class="info">
@@ -475,7 +308,7 @@
     <div class="panel-heading" align="center">
      <h2 style="margin-top:5px; float:center">Report An Issue</h2>
     </div>
-        <form class="form-horizontal" role="form" method="POST" action="single_issue.php"
+	<form class="form-horizontal" role="form" method="POST" action="single_issue.php"
       target='_blank'>
     <div class="panel panel-default">
      <div class="panel-body">
@@ -499,14 +332,14 @@
         <label for="section" class="col-sm-2 control-label">Section</label>
         <div class="col-sm-4">
          <select id="section" name="section" class="form-control">
-                  <option>101</option>
+		  <option>101</option>
           <option>102</option>
           <option>201</option>
-                  <option>202</option>
-                  <option>L1A</option>
+		  <option>202</option>
+		  <option>L1A</option>
           <option>L1B</option>
           <option>L1C</option>
-                  <option>L2A</option>
+		  <option>L2A</option>
           <option>L2B</option>
           <option>L3C</option>
           <option>T1A</option>
@@ -569,13 +402,13 @@
          </select>
         </div>
        </div>       
-     </div>         
+     </div>	 
     </div>
-        <div class="row" align="right">
+	<div class="row" align="right">
         <button type="submit" class="btn btn-default">Submit</button>
-                <button type="reset" class="btn btn-default">Reset</button>
+		<button type="reset" class="btn btn-default">Reset</button>
    </div>
-        </form>
+	</form>
    </div>
    
   </div>
@@ -585,13 +418,13 @@
   <script src="vendor/bootstrap/js/bootstrap.js" type="text/javascript"></script>
   <script src="js/app.js" type="text/javascript"></script>
   <script>  
-        $(function ()  
-        { 
-                $("#recentActivityPopOver").popover({placement:'bottom', trigger: 'click', html:true,
-                content: function() {
+	$(function ()  
+	{ 
+		$("#recentActivityPopOver").popover({placement:'bottom', trigger: 'click', html:true,
+		content: function() {
           return $('#recentActivity_popover_content_wrapper').html();
         }});  
-        });  
+	});  
   </script>  
  </div>
 </body>
