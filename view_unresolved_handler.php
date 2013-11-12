cc28dc803955a715f2f923d0cb9bc4b8be851995
Jae-Hwan (Jeff)  Jung <midlife83@gmail.com>
2013-11-11 22:35:52 -0800

    Minor changes. This prototype is pretty much done now.

--- view_unresolved_handler.php
+++ view_unresolved_handler.php
@@ -6,7 +6,7 @@ session_start();
 		$_SESSION['number']='53212178'; 
 		$_SESSION['section']='L1B'; 
 		$_SESSION['title']='Jane keeps falling asleep'; 
-		$_SESSION['description']='THIS PAGE IS JUST A SAMPLE PAGE TO SHOW HOW IT WOULD LOOK LIKE. THE INFORMATION SHOWN HERE MAY BE DIFFERENT FROM THE ISSUE YOU JUST CLICKED.'; 
+		$_SESSION['description']='THIS PAGE IS JUST A SAMPLE PAGE TO SHOW HOW IT WOULD LOOK LIKE.<br>THE INFORMATION SHOWN HERE MAY BE DIFFERENT FROM THE ISSUE YOU JUST CLICKED.'; 
 		$_SESSION['assignment']='[TA] Erica Wilbur'; 
 		$_SESSION['label']='Lab2';
 		$_SESSION['comment']=NULL; 
